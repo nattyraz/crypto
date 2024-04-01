9fb7ce10c21aafab70ff88845a882997cf719d1c
ROFOX <rgfogliami@gmail.com>
2024-04-02 00:45:57 +0200

    Add files via upload

---
+++ DCBD.js
@@ -0,0 +1,8 @@
+{
+    "name": "The Druid CBD",
+    "symbol": "DCBD",
+    "description": "The Druid CBD Token embodies nature and Celtic magic in a playful digital world. Celebrating harmony with organic and natural elements, this token is a passport for adventures where each participant cultivates their mystical garden. It unites a community in search of fun, exchange, and discovery within a vibrant, green ecosystem.",
+    "decimals": 9,
+    "logo": "https://raw.githubusercontent.com/nattyraz/crypto/main/BCBD2%400.5x.png?token=GHSAT0AAAAAACQBQGWJWURQ35ZPYNWHVPNOZQLEJCA"
+  }
+  
